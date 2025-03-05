98df66d6155b596d0ef1f756793cd564d7788d63
Shawon Hossain <muhammadshawonhossain.msh@gmail.com>
2025-03-05 23:34:40 +0600

    practice some important path modules

---
+++ Chapter-02-PathModule/path.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// const newPath = path.join(__dirname , "New Folder", "newFile.js");
+// console.log(newPath);
+
+// console.log(path.extname(__filename));
+console.log(path.basename(__filename));
