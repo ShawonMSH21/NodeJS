a1bc699e9cbf8ca5b6f1c60e8bb72f7dd06869a9
Shawon Hossain <muhammadshawonhossain.msh@gmail.com>
2025-03-05 22:41:20 +0600

    practiced import and export modules in Chapter 01

---
+++ Chapter01-Modules/app.js
@@ -0,0 +1,10 @@
+// console.log("Assalamu Alaikum");
+// const add = require('./math');
+// const {add, sub} = require('./math');
+const math = require('./math');
+
+// add(21, 32);
+// sub(21, 32);
+
+math.add(21, 20);
+math.sub(21, 20);
