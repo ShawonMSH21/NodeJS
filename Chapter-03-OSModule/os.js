678992025b0691a3ea021226190f88cb844dbe78
Shawon Hossain <muhammadshawonhossain.msh@gmail.com>
2025-03-06 00:10:17 +0600

    OS Module Loaded

---
+++ Chapter-03-OSModule/os.js
@@ -0,0 +1,13 @@
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.userInfo());
+console.log(os.type());
+console.log(os.hostname());
+console.log(os.cpus());
+console.log(os.uptime());
+console.log(os.homedir());
+console.log(os.totalmem());
+console.log(os.freemem());
+
