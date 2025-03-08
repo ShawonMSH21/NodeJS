209474eb6d337c5e1244f1c18f847a8db7851269
Shawon Hossain <156779561+ShawonMSH21@users.noreply.github.com>
2025-03-08 19:42:43 +0600

    play with ES6 Export Import Features

---
+++ Chapter-07-ES6ModuleExportImport/export.js
@@ -0,0 +1,17 @@
+const addition = (num1, num2) => {
+    console.log(num1+num2);
+}
+
+const multiplication = (num1, num2) => {
+    console.log(num1 * num2);
+}
+
+export const modulus = (num1, num2) => {
+    console.log(num1%num2);
+}
+
+//default export
+// export default multiplication;
+
+// Named or others way
+// export {addition, multiplication}
---
+++ Chapter-07-ES6ModuleExportImport/import.js
@@ -0,0 +1,13 @@
+//deafult import 
+// import multiplication from "./export.js";
+
+// Others Way to import
+// import { addition, multiplication } from "./export.js";
+// import { modulus } from "./export.js";
+import * as math from "./export.js"
+
+// multiplication(21, 20);
+// addition(21, 20);
+// modulus(21, 20);
+math.modulus(21, 20);
+
